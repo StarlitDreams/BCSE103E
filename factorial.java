f3b983769e79362d145015a37dc4fa47b9aec5e1
Hari <kharinarayanan.a2022@vitstudent.ac.in>
2023-07-25 08:41:16 +0530

    created factorial

---
+++ factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class factorial {
+    public int fact(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        factorial factorial = new factorial();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the value you want to find the factorial of:");
+        int num=myObj.nextInt();
+        int result = factorial.fact(num);
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+}
