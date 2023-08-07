932d58b29038d217931e20dba7aed54a01d4fb51
Hari <kharinarayanan.a2022@vitstudent.ac.in>
2023-08-07 18:23:04 +0530

    q7

--- Q7_array.java
+++ Q7_array.java
@@ -1,3 +1,36 @@
+/*
+
+Consider First n even numbers starting from zero(0).Complete the code segment to calculate sum of  all the numbers divisible by 3 from 0 to n. Print the sum.
+
+
+Example:
+
+Input: n = 5
+
+-------
+0 2 4 6 8
+Even number divisible by 3:0 6
+sum:6
+
+
+--- Input ---
+
+5
+0 2 4 6 8
+
+
+--- Program output ---
+
+================================
+0 2 4 6 8
+
+
+--- Expected output (numbers)---
+
+0 6
+6
+
+*/
 import java.util.*;
 
 public class Q7_array {
@@ -9,12 +42,15 @@ public class Q7_array {
         for(int i=0;i<n;i++){
             list.add(myobj.nextInt());
         }
+    int sum=0;
     System.out.println("================================");
         for(int x:list){
-            if(x%2==0){
+            if(x%3==0){
                 System.out.printf("%d ",x);
+                sum+=x;
             }
         }
+        System.out.printf("%d ",sum);
 
 
 
